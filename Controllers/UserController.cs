72078854a92a196f3b8c38d2b02ff2e15ccfb0b6
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-07-07 23:11:08 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,103 @@
+﻿using apiBodega.Data;
+using apiBodega.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiBodega.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDBContext _db;
+        public UserController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<User> users = await _db.usuarios.ToListAsync();
+            return Ok(users);
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{IdUser}")]
+        public async Task<IActionResult> Get(int IdUser)
+        {
+            User user = await _db.usuarios.FirstOrDefaultAsync(x => x.IdUser == IdUser);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            return BadRequest();
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User usuario)
+        {
+
+            // En caso de que el framework no valide que ya exista un ID que ya existe debemos validar nosotros
+            User existingUser = await _db.usuarios.FirstOrDefaultAsync(x => x.UserMail== usuario.UserMail && x.UserPassword == usuario.UserPassword);
+
+            //var existingUser = _db.usuarios.FirstOrDefaultAsync(u => u.UserMail == existingUser.UserMail && u.UserPassword == existingUser.UserPassword);
+            if (existingUser != null)
+            {
+                return Ok(existingUser);
+            }
+            else
+            {
+                return NotFound("El usuario no existe.");
+            }
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateUser([FromBody] User newUser)
+        {
+            User nuevouser = await _db.usuarios.FirstOrDefaultAsync(x => x.IdUser == newUser.IdUser);
+            if (nuevouser == null && newUser != null)
+            {
+                await _db.usuarios.AddAsync(newUser);
+                await _db.SaveChangesAsync();
+                return Ok(newUser);
+            }
+
+            return BadRequest("No se creo correctamente el  usuario");
+        }
+
+        [HttpDelete("{IdUser}")]
+        public async Task<IActionResult> Delete(int IdUser)
+        {
+            User usuario = await _db.usuarios.FirstOrDefaultAsync(x => x.IdUser == IdUser);
+            if (usuario != null)
+            {
+                _db.usuarios.Remove(usuario);
+                await _db.SaveChangesAsync();
+                return Ok("Usuario Eliminado Correctamente");
+            }
+            return BadRequest("No se logró eliminar al usuario");
+        }
+
+        /*[HttpGet]
+        [Route("api/User/VerifyCredentials")]
+        public async Task<IActionResult> VerifyCredentials(string usermail, string userpassword)
+        {
+            // Lógica para verificar las credenciales en tu base de datos o sistema de autenticación
+            var user = _db.usuarios.FirstAsync(u=> u.UserMail == usermail && u.UserPassword==userpassword);
+
+            if (user != null && usermail==)
+            {
+                return Ok("El usuario SI existe."); // Retorna el usuario si las credenciales son correctas
+            }
+            else
+            {
+                return NotFound("No existe usuario"); // Retorna un código 404 si las credenciales son incorrectas o el usuario no existe
+            }*/
+        
+
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,113 @@
+﻿using apiBodega.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+
+namespace apiBodega.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext( 
+            DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> usuarios { get; set; }
+        public DbSet<Empresa> empresas { get; set; }
+        public DbSet<Producto> products { get; set; }
+        public DbSet<Pedidos>  pedidos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+
+                new User
+                {
+                    IdUser = 1,
+                    UserMail = "rick1234@gmail.com",
+                    UserPassword = "admin4321",
+                    
+                },
+                
+                new User
+                {
+                    IdUser = 2,
+                    UserMail = "cris1234@gmail.com",
+                    UserPassword = "admin1234",
+                    
+                });
+
+            modelBuilder.Entity<Empresa>().HasData(
+
+                new Empresa
+                {
+                    EmpresaID = 1,
+                    NombreEmpresa = "Supermaxi",
+                    Resumen = "Empresa de viveres y consumo"
+
+                },
+                new Empresa
+                {
+                    EmpresaID = 2,
+                    NombreEmpresa = "ElectronicaEc",
+                    Resumen = "Empresa que vende componentes eléctricos"
+
+                });
+            modelBuilder.Entity<Producto>().HasData(
+                new Producto
+                {
+                    ProductoId = 1,
+                    Nombre = "Clorox",
+                    Descripcion = "Cloro para uso doméstico",
+                    Precio = 5.99,
+                    CtdenStock = 300,
+                    ProveedorId = 1,
+
+                },
+                new Producto
+                {
+                    ProductoId = 2,
+                    Nombre = "Lava",
+                    Descripcion = "Detergente en crema ideal para el lavado manual de vajilla",
+                    Precio = 6.99,
+                    CtdenStock = 89,
+                    ProveedorId = 1,
+
+                },
+                new Producto
+                {
+                    ProductoId = 3,
+                    Nombre = " Kit Brochas antiestáticas",
+                    Descripcion = "Brochas para limpieza de componentes eléctricos",
+                    Precio = 3.50,
+                    CtdenStock = 4,
+                    ProveedorId = 2,
+
+
+                },
+                new Producto
+                {
+                    ProductoId = 4,
+                    Nombre = "Alcohol Isopropilico",
+                    Descripcion = "Alcohol Isopropilico al 90%",
+                    Precio = 6.80,
+                    CtdenStock = 34,
+                    ProveedorId = 2,
+                }
+                );
+            modelBuilder.Entity<Pedidos>().HasData(
+                
+                new Pedidos
+                {
+                    ClienteId = 1,
+                    CtdProducto = 54,
+                    ProveedorId = 2,
+                }
+                
+                
+                
+                );
+            }
+        
+    }
+}
